8939d41a16bdc9461dab7df7e845864c8e7703b3
Liam Naylor <liam@liamnaylor.com>
2023-03-02 14:00:08 -0800

    Add employees to store 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Liam", last_name: "Naylor", hourly_rate: 65)
+@store1.employees.create(first_name: "Abigail", last_name: "Morse", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Kobe", last_name: "Bryant", hourly_rate: 55)
+@store2.employees.create(first_name: "Lebron", last_name: "James", hourly_rate: 67)
+@store2.employees.create(first_name: "Penny", last_name: "Hardaway", hourly_rate: 62)
