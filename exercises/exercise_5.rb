3f68ecd62fc967bf00d4fbb524ec222f9bbc0aa5
Liam Naylor <liam@liamnaylor.com>
2023-03-02 13:51:57 -0800

    Add exercise 1-6 + add associations to classes

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_sales = Store.sum(:annual_revenue)
+@avg_sales = Store.average(:annual_revenue).to_i
+@total_large_stores = Store.where(annual_revenue: 1000000..).count
+
+puts "Total sales: $#{@total_sales}"
+puts "Average sales: $#{@avg_sales}"
+puts "Number of stores >$1M in sales: #{@total_large_stores}"
